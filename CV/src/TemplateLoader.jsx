c12f2af71d0d3e02c2c84561b978b9b267e6fd6c
GeorgeOprea <oprea.george14@yahoo.com>
2024-02-22 12:09:10 +0200

    added load example button

--- CV/src/TemplateLoader.jsx
+++ CV/src/TemplateLoader.jsx
@@ -4,15 +4,20 @@ import { FaRegFilePdf } from "react-icons/fa";
 
 const TemplateLoader = (props) => {
   return (
-    <div className="template-loader">
-      <button className="clear-resume" onClick={props.deleteAll}>
-        <FaTrashAlt />
-        Delete All
-      </button>
-      <button className="download" onClick={props.onDownload}>
-        <FaRegFilePdf /> Download
-      </button>
-    </div>
+    <>
+      <div className="template-loader">
+        <button className="clear-resume" onClick={props.deleteAll}>
+          <FaTrashAlt />
+          Delete All
+        </button>
+        <button className="download" onClick={props.onDownload}>
+          <FaRegFilePdf /> Download
+        </button>
+      </div>
+      <div className="load-example-container">
+        <button id="load-example">Load Example</button>
+      </div>
+    </>
   );
 };
 
