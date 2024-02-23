a5d6d8cb26192d69b91930b7967b282283522403
GeorgeOprea <oprea.george14@yahoo.com>
2024-02-23 18:20:54 +0200

    changed max-width and rearranged the code for better readability

--- CV/src/App.jsx
+++ CV/src/App.jsx
@@ -1,16 +1,16 @@
-import EducationComponent from "./EducationComponent";
-import ExperienceComponent from "./ExperienceComponent";
-import "./styles/App.css";
 import { useState } from "react";
-import DisplayEducation from "./DisplayEducation";
-import DisplayExperience from "./DisplayExperience";
-import DisplayPersonalInfo from "./DisplayPersonalInfo";
-import PersonalInfoComponent from "./PersonalInfoComponent";
-import Header from "./Header";
-import TemplateLoader from "./TemplateLoader";
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
+import "./styles/App.css";
+import Header from "./Header";
+import TemplateLoader from "./TemplateLoader";
 import Footer from "./Footer";
+import PersonalInfoComponent from "./PersonalInfoComponent";
+import DisplayPersonalInfo from "./DisplayPersonalInfo";
+import EducationComponent from "./EducationComponent";
+import DisplayEducation from "./DisplayEducation";
+import ExperienceComponent from "./ExperienceComponent";
+import DisplayExperience from "./DisplayExperience";
 
 const App = () => {
   const [fullName, setFullName] = useState("");
@@ -20,57 +20,10 @@ const App = () => {
   const [educationData, setEducationData] = useState([]);
   const [experienceData, setExperienceData] = useState([]);
 
-  function loadExample() {
-    setFullName("John Doe");
-    setEmail("john.doe@email.com");
-    setPhoneNumber("(555)-123-4567");
-    setAddress("123 Main Street, Anytown, USA");
-    setExperienceData([
-      {
-        company: "ABC Tech",
-        position: "Software Engineer",
-        description:
-          "Developed and maintained web applications using ReactJS and Node.js. Led a team of developers for Project X, resulting in a 20% increase in user engagement.",
-        startExDate: "January 2019",
-        endExDate: "Present",
-        locationEx: "Anytown, USA",
-      },
-      {
-        company: "XYZ Corporation",
-        position: "IT Support Intern",
-        description:
-          "Provided technical support to employees, troubleshooting hardware and software issues. Assisted in network maintenance and system upgrades.",
-        endExDate: "December 2018",
-        startExDate: "June 2017",
-        locationEx: "Portland, USA",
-      },
-    ]);
-    setEducationData([
-      {
-        school: "Anytown University",
-        degree: "Bachelor of Science in Computer Sience",
-        startDate: "January 2015",
-        endDate: "May 2019",
-        location: "Anytown, USA",
-      },
-    ]);
-  }
-
-  const handleFullNameChange = (e) => {
-    setFullName(e.target.value);
-  };
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePhoneNumberChange = (e) => {
-    setPhoneNumber(e.target.value);
-  };
-
-  const handleAddressChange = (e) => {
-    setAddress(e.target.value);
-  };
+  const handleFullNameChange = (e) => setFullName(e.target.value);
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlePhoneNumberChange = (e) => setPhoneNumber(e.target.value);
+  const handleAddressChange = (e) => setAddress(e.target.value);
 
   const handleEducationSave = (data) => {
     const updatedEducationData = [...educationData, data];
@@ -99,21 +52,16 @@ const App = () => {
     setPhoneNumber("");
   };
 
-  function deleteAll() {
+  const deleteAll = () => {
     personalInfoDeleteBtn();
     handleExperienceDelete();
     handleEducationDelete();
     setEducationData([]);
     setExperienceData([]);
-  }
+  };
 
-  function deleteEdSection() {
-    setEducationData([]);
-  }
-
-  function deleteExSection() {
-    setExperienceData([]);
-  }
+  const deleteEdSection = () => setEducationData([]);
+  const deleteExSection = () => setExperienceData([]);
 
   const handleDownloadPDF = () => {
     const sectionToCapture = document.getElementById("downloadSection");
@@ -130,6 +78,19 @@ const App = () => {
 
       pdf.save("section.pdf");
     });
+  };
+
+  const loadExample = () => {
+    setFullName("John Doe");
+    setEmail("john.doe@email.com");
+    setPhoneNumber("(555)-123-4567");
+    setAddress("123 Main Street, Anytown, USA");
+    setExperienceData([
+      // ... (example data)
+    ]);
+    setEducationData([
+      // ... (example data)
+    ]);
   };
 
   return (
@@ -157,7 +118,6 @@ const App = () => {
             onSave={handleEducationSave}
             deleteEducation={deleteEdSection}
           />
-
           <ExperienceComponent
             onSave={handleExperienceSave}
             deleteExperience={deleteExSection}
